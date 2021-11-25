40a53e18594bc18b48cced73dd9aaecad7646165
KariiimFouad <94301032+KariiimFouad@users.noreply.github.com>
2021-11-25 20:11:18 +0200

    Sign_up

---
+++ Class_SignUp.dart
@@ -0,0 +1,14 @@
+class  SignUp {
+
+  String? firstname;
+  String? lastname;
+  dynamic? email;
+
+ SignUp(this.firstname,this.email, {required this.lastname});
+}
+
+
+
+
+
+
---
+++ sign_up.dart
@@ -0,0 +1,18 @@
+import 'Class_SignUp.dart';
+
+void main(List<String> arguments) {
+
+  var user1 = SignUp ("Karim","kariiimfouad@gmail.com",lastname: "fouad");
+
+  print("user 1 name:${user1.firstname},his second name is ${user1.lastname}, his email is ${user1.email}");
+
+  var user2 = SignUp ("Ahmed","ahmed38fouad@gmail.com",lastname: "fouad");
+
+  print("user 2 name:${user2.firstname},his second name is ${user2.lastname}, his email is ${user2.email}");
+
+
+}
+
+
+
+
